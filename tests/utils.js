afc9441eab00eada36828b1c991e9bb472174edf
tianshengjie <tianshengjie@howzhi.com>
2018-03-29 11:54:13 +0800

    修复jsdom，jquery全局影响

--- tests/utils.js
+++ tests/utils.js
@@ -1,6 +1,9 @@
-let { JSDOM } = require('jsdom');
 
+const decache = require('decache');
 exports.initJquery = function(html, params = {}){
+    decache('jsdom');
+    decache('jquery');
+    let { JSDOM } = require('jsdom');
     params = Object.assign({
         url: 'http://127.0.0.1',
         referrer: 'http://127.0.0.1',
@@ -9,6 +12,7 @@ exports.initJquery = function(html, params = {}){
         includeNodeLocations: true,
     }, params);
     let dom = new JSDOM(`<!DOCTYPE html><html><body>${html}</body></html>`, params);
+
     global.window = dom.window;
     global.$ = require('jquery');
 }
